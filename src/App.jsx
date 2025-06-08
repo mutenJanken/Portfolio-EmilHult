2069534937bbf3e588f42ae213c9d5a4fa4bbace
emil hult <Emilhult@live.com>
2025-06-08 20:54:16 +0200

    Added a footer + other minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,40 @@ import Aboutme from "./components/Aboutme";
 import { NavLink, Routes, Route, Navigate } from "react-router-dom";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
 
-      <div className="container">
+      <div className="wrapper">
         {/* NAV LINKS / TABS  */}
-        <div className="navlinks-wrapper">
-          <NavLink className="navlink-tab" to="/about">
-            About me
-          </NavLink>
-          <NavLink className="navlink-tab" to="/skills">
-            Skills
-          </NavLink>
-          <NavLink className="navlink-tab" to="/portfolio">
-            Portfolio
-          </NavLink>
-        </div>
+        <main>
+          <div className="navlinks-wrapper">
+            <NavLink className="navlink-tab" to="/about">
+              About me
+            </NavLink>
+            <NavLink className="navlink-tab" to="/skills">
+              Skills
+            </NavLink>
+            <NavLink className="navlink-tab" to="/portfolio">
+              Portfolio
+            </NavLink>
+          </div>
 
-        {/* ROUTES  */}
-        <Routes>
-          {/* HOME PAGE */}
-          <Route path="/" element={<Navigate to="/about" replace />} />
+          {/* ROUTES  */}
+          <Routes>
+            {/* HOME PAGE */}
+            <Route path="/" element={<Navigate to="/about" replace />} />
 
-          <Route path="/about" element={<Aboutme />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
+            <Route path="/about" element={<Aboutme />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </main>
+
+        <Footer></Footer>
       </div>
     </>
   );
