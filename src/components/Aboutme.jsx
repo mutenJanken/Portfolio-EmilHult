f14a37925fb68cbf00ca0629d524e304d9d48d45
emil hult <Emilhult@live.com>
2025-06-09 16:27:34 +0200

    Fixed about me layout

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -8,14 +8,25 @@ function Aboutme() {
         <div className="card-under-one">UNDER1</div>
         <div className="card-under-two">UNDER2</div>
         <section>
-          <div className="profile-picture-wrapper">
-            <img className="profile-picture" src={ProfilePicture} alt="Profile picture" />
+          <div className="about-me-wrapper">
+            <div className="profile-picture-wrapper">
+              <img className="profile-picture" src={ProfilePicture} alt="Profile picture" />
+            </div>
+            <div className="about-me-vertical">
+              <h2 className="greeting-text">Hello, my name is Emil Hult</h2>
+              <p className="aboutme-description-wrapper">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur, doloremque. Repellendus
+                consequatur beatae eius illo, molestiae unde quos libero cumque obcaecati corrupti ad in
+                deleniti tempore recusandae animi nobis ea!
+              </p>
+              <div className="intrest-horizontal">
+                <div className="intrest">Developer</div>
+                <div className="intrest">Bookworm</div>
+                <div className="intrest">Guitar</div>
+                <div className="intrest">Exercise</div>
+              </div>
+            </div>
           </div>
-          <p className="aboutme-description-wrapper">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur, doloremque. Repellendus
-            consequatur beatae eius illo, molestiae unde quos libero cumque obcaecati corrupti ad in deleniti
-            tempore recusandae animi nobis ea!
-          </p>
         </section>
       </div>
     </>
