5bc323a9c41525319d15136388fe1c420a3bfa69
emil hult <Emilhult@live.com>
2025-06-14 21:18:49 +0200

    Minor fixes

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink, Routes, Route, Navigate } from "react-router-dom";
+
+
+function NavBar() {
+  return (
+    <div className="navlinks-wrapper">
+      <NavLink className="navlink-tab" to="/about">
+        About me
+      </NavLink>
+      <NavLink className="navlink-tab" to="/skills">
+        Skills
+      </NavLink>
+      <NavLink className="navlink-tab" to="/portfolio">
+        Portfolio
+      </NavLink>
+    </div>
+  );
+}
+
+export default NavBar;
