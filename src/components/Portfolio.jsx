dc2a06a5fc9b9c962373459d3c4bff2f4b987bb1
emil hult <Emilhult@live.com>
2025-06-19 15:41:28 +0200

    new border style for Project-header and skill-header(skill-h5)

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -28,25 +28,6 @@ function Portfolio() {
               </div>
             </a>
 
-            {/* MOVIE BOOKING  */}
-            <a className="http-link" href="https://github.com/mutenJanken/movie-theater">
-              <div className="project-wrapper">
-                <div className="project-description-wrapper">
-                  <h2 className="project-header">Movie theater</h2>
-                  <p className="project-description">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas nisi maxime quisquam!
-                  </p>
-                </div>
-                <div className="project-image-wrapper">
-                  <img
-                    className="project-image"
-                    src={MovieBookingProjectImage}
-                    alt="Movie booking Project Image"
-                  />
-                </div>
-              </div>
-            </a>
-
             {/* BOOKING MICROSERVICE */}
             <a className="http-link" href="https://github.com/mutenJanken/BookingServiceProvider">
               <div className="project-wrapper">
@@ -79,6 +60,25 @@ function Portfolio() {
                   <img
                     className="project-image"
                     src={EmailMicroserviceImage}
+                    alt="Movie booking Project Image"
+                  />
+                </div>
+              </div>
+            </a>
+
+            {/* MOVIE BOOKING  */}
+            <a className="http-link" href="https://github.com/mutenJanken/movie-theater">
+              <div className="project-wrapper">
+                <div className="project-description-wrapper">
+                  <h2 className="project-header">Movie theater</h2>
+                  <p className="project-description">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas nisi maxime quisquam!
+                  </p>
+                </div>
+                <div className="project-image-wrapper">
+                  <img
+                    className="project-image"
+                    src={MovieBookingProjectImage}
                     alt="Movie booking Project Image"
                   />
                 </div>
